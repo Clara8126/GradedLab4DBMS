628cbaa6aa2ab75265ec8b84bb99b83f72142f20
Clara8126 <101538189+Clara8126@users.noreply.github.com>
2022-05-28 03:53:49 +0530

    Update e_com.sql

--- e_com.sql
+++ e_com.sql
@@ -111,42 +111,129 @@ INSERT INTO SUPPLIER_PRICING VALUES(2,3,5,30000);
 INSERT INTO SUPPLIER_PRICING VALUES(3,5,1,3000);
 INSERT INTO SUPPLIER_PRICING VALUES(4,2,3,2500);
 INSERT INTO SUPPLIER_PRICING VALUES(5,4,1,1000);
+INSERT INTO SUPPLIER_PRICING VALUES(6,12,2,780);
+INSERT INTO SUPPLIER_PRICING VALUES(7,12,4,789);
+INSERT INTO SUPPLIER_PRICING VALUES(8,3,1,31000);
+INSERT INTO SUPPLIER_PRICING VALUES(9,1,5,1450);
+INSERT INTO SUPPLIER_PRICING VALUES(10,4,2,999);
+INSERT INTO SUPPLIER_PRICING VALUES(11,7,3,549);
+INSERT INTO SUPPLIER_PRICING VALUES(12,7,4,529);
+INSERT INTO SUPPLIER_PRICING VALUES(13,6,2,105);
+INSERT INTO SUPPLIER_PRICING VALUES(14,6,1,99);
+INSERT INTO SUPPLIER_PRICING VALUES(15,2,5,2999);
+INSERT INTO SUPPLIER_PRICING VALUES(16,5,2,2999);
 
 
-INSERT INTO `ORDER` VALUES (101,1500,2021-10-06,2,1);
-INSERT INTO `ORDER` VALUES (102  ," 1000",2021-10-12,"  3","5");
-INSERT INTO `ORDER` VALUES (103  ," 30000",2021-09-16,"  5"," 2");
-INSERT INTO `ORDER` VALUES (104 ," 1500",2021-10-05," 1","  1");
-INSERT INTO `ORDER` VALUES (105 ," 3000 ",2021-08-16,"  4 "," 3");
-INSERT INTO `ORDER` VALUES (106 ,1450,2021-08-18,1,9);
-INSERT INTO `ORDER` VALUES (107 ," 789 ",2021-09-01," 3 "," 7");
-INSERT INTO `ORDER` VALUES (108 ," 780 ",2021-09-07 ," 5","  6");
-INSERT INTO `ORDER` VALUES (109 ," 3000 ",2021-00-10 ," 5 "," 3");
-INSERT INTO `ORDER` VALUES (110 ," 2500",2021-09-10,"  2 "," 4");
-INSERT INTO `ORDER` VALUES (111 ," 1000 ",2021-09-15,"  4","  5");
-INSERT INTO `ORDER` VALUES (112 ," 789 ",2021-09-16,"  4 "," 7");
-INSERT INTO `ORDER` VALUES (113 ," 31000",2021-09-16,"  1"," 8");
-INSERT INTO `ORDER` VALUES (114,"  1000",2021-09-16," 3","  5");
-INSERT INTO `ORDER` VALUES (115 ," 3000",2021-09-16," 5 "," 3");
-INSERT INTO `ORDER` VALUES (116," 99",2021-09-17,"  2","14");
+
+INSERT INTO `ORDER` VALUES (101,1500,'2021-10-06', 2, 1);
+INSERT INTO `ORDER` VALUES (102  ,1000,2021-10-12,3,5);
+INSERT INTO `ORDER` VALUES (103  ," 30000",'2021-09-16',"  5"," 2");
+INSERT INTO `ORDER` VALUES (104 ," 1500",'2021-10-05'," 1","  1");
+INSERT INTO `ORDER` VALUES (105 ," 3000 ",'2021-08-16',"  4 "," 3");
+INSERT INTO `ORDER` VALUES (106 ,1450,'2021-08-18',1,9);
+INSERT INTO `ORDER` VALUES (107 ," 789 ",'2021-09-01'," 3 "," 7");
+INSERT INTO `ORDER` VALUES (108 ," 780 ",'2021-09-07' ," 5","  6");
+INSERT INTO `ORDER` VALUES (109 ," 3000 ",'2021-00-10' ," 5 "," 3");
+INSERT INTO `ORDER` VALUES (110 ," 2500",'2021-09-10',"  2 "," 4");
+INSERT INTO `ORDER` VALUES (111 ," 1000 ",'2021-09-15',"  4","  5");
+INSERT INTO `ORDER` VALUES (112 ," 789 ",'2021-09-16',"  4 "," 7");
+INSERT INTO `ORDER` VALUES (113 ," 31000",'2021-09-16',"  1"," 8");
+INSERT INTO `ORDER` VALUES (114,"  1000",'2021-09-16'," 3","  5");
+INSERT INTO `ORDER` VALUES (115 ," 3000",'2021-09-16'," 5 "," 3");
+INSERT INTO `ORDER` VALUES (116," 99",'2021-09-17',"  2","14");
 
 
-INSERT INTO RATING VALUES(1 ,101 ,4);
-INSERT INTO RATING VALUES(12, 102,3);
-INSERT INTO RATING VALUES(13,103 ,1);
-INSERT INTO RATING VALUES(4 ,104 ,2);
+INSERT INTO RATING VALUES(1,101,4);
+INSERT INTO RATING VALUES(2, 102,3);
+INSERT INTO RATING VALUES(3,103 ,1);
+INSERT INTO RATING VALUES(4,104,2);
 INSERT INTO RATING VALUES(5 ,105 ,4);
 INSERT INTO RATING VALUES(6 ,106 ,3);
 INSERT INTO RATING VALUES(7 ,107, 4);
 INSERT INTO RATING VALUES(8 ,108, 4);
 INSERT INTO RATING VALUES(9 ,109, 3);
-INSERT INTO RATING VALUES(10 ,110, 5);
-INSERT INTO RATING VALUES(11 ,111 ,3);
-INSERT INTO RATING VALUES(12 ,112 ,4);
-INSERT INTO RATING VALUES(13 ,113 ,2);
+INSERT INTO RATING VALUES(10,110, 5);
+INSERT INTO RATING VALUES(11, 111, 3);
+INSERT INTO RATING VALUES(12,112,4);
+INSERT INTO RATING VALUES(13,113,2);
 INSERT INTO RATING VALUES(14 ,114 ,1);
 INSERT INTO RATING VALUES(15, 115 ,1);
 INSERT INTO RATING VALUES(16 ,116 ,0);
 
+/* To display  total number of customers based on gender who have placed orders of worth at least Rs.3000*/
+
+select CUS_GENDER as GENDER, count(distinct(CUS_ID)) AS NoofCustomers from 
+(select customer.CUS_ID, customer.CUS_GENDER from customer INNER JOIN
+
+(select CUS_ID from e_com.order where ORD_AMOUNT >=3000) AS filter_order
+on customer.CUS_ID = filter_order.CUS_ID) as filter_customer group by CUS_GENDER;
 
 
+/* To display all the orders along with the product name ordered by customer having customer_id = 2 */
+
+select order_details.*,prod.pro_name from product as prod INNER JOIN 
+(select ord.*, price.PRO_ID from e_com.order as ord inner join supplier_pricing as price
+ on ord.PRICING_ID = price.PRICING_ID where ord.cus_id = 2) as order_details 
+ on order_details.PRO_ID = prod.pro_id;
+ 
+ select product.PRO_NAME, `order`.* from `order` , supplier_pricing , product 
+ where `order`.CUS_ID =2 and
+`order`.pricing_id = supplier_pricing.PRICING_ID and 
+ supplier_pricing.PRO_ID = product.PRO_ID;
+ 
+ /* Display Supplier details who can supply more than one product.*/
+ 
+ select supplier.* from supplier where supplier.SUPP_ID in 
+ (select SUPP_ID from supplier_pricing group by SUPP_ID having 
+ count(PRO_ID) >= 1);
+ 
+ /* To Display  least expensive product from each category and print the table 
+ with category id, name, product name and price of the product */
+ 
+ select category.cat_id, category.cat_name, min(t3.min_price) as Min_Price from category INNER JOIN
+ (select product.cat_id, product.pro_name, t2.*from product inner join
+ (select pro_id, min(supp_price) as Min_Price from supplier_pricing group by pro_id)
+ as t2 where t2.pro_id = product.pro_id )
+ as t3 where t3.cat_id = category.cat_id group by t3.cat_id;
+ 
+ /* To Display the Id and Name of the Product ordered after “2021-10-05”*/
+ select PRO_ID , PRO_NAME from product where pro_id in 
+ (select pro_id from supplier_pricing where PRICING_ID in 
+ (select PRICING_ID from e_com.order where ord_data > '2021-10-05'));
+ 
+ 
+ select product.pro_id, product.pro_name from `order` inner join 
+ supplier_pricing on supplier_pricing.PRICING_ID = `order`.pricing_id inner join product 
+ on product.pro_id = supplier_pricing .prod_id where `order`.ord_date> "2021-10-05";
+
+ 
+/* to display customer name and gender whose names start or end with characher 'A'*/
+
+select customer.cus_name , customer.cus_gender from customer where 
+customer.cus_name like '%A' or customer.cus_name like '%A';
+
+/* Create a stored procedure to display supplier id, name, rating and Type_of_Service. For Type_of_Service, If rating =5, print “Excellent 
+Service”,If rating >4 print “Good Service”, If rating >2 print “Average Service” else print “Poor Service” */
+
+DELIMITER &&
+CREATE PROCEDURE proc()
+BEGIN 
+select report.supp_id, report.supp_name,report.Average,
+CASE
+WHEN report.Average = 5 THEN 'Excellect Service'
+WHEN report.Average = 5 THEN 'Good Service'
+WHEN report.Average = 5 THEN 'Average Service'
+ELSE 'Poor Service'
+END AS Type_of_Service from 
+(select final.supp_id ,supplier.supp_name,final.Average from 
+(select test2.supp_id, sum(test2.rat_ratstars)/count(test2.rat_ratstars) as Average from 
+(select supplier_pricing.supp_id,test.ORD_ID, test.RAT_RATSTARS from  supplier_pricing inner join 
+(select `order`.PRICING_ID,rating.ORD_ID, rating.RAT_RATSTARS from `order` inner join
+ rating on rating.`ord_id` = `order`.ORD_ID) as test
+on test.pricing_id = supplier_pricing.pricing_id)
+as test2 group by supplier_pricing.supp_id)
+as final inner join supplier where final.supp_id = supplier.supp_id) as report;
+END &&
+DELIMITER ;
+
+call proc();
